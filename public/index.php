2c3f51d93f13a719d3afb874ceee8d5c08dcb847
An. Vu Ngoc - CMC Global DU1.18 <vnan1@cmcglobal.vn>
2022-05-20 23:59:41 +0700

    Update

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ if(!empty($_SERVER['REQUEST_URI'])){
 		}
 	}
 }
-if (!version_compare(phpversion(), '7.2', '>'))
-{
-    die("Current PHP version: ".phpversion()."<br>You must upgrade PHP version 7.2 and later");
-}
+// if (!version_compare(phpversion(), '7.2', '>'))
+// {
+//     die("Current PHP version: ".phpversion()."<br>You must upgrade PHP version 7.2 and later");
+// }
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
